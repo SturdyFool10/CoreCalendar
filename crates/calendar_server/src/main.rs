91ec7aa059fec38adadc97c670fe28e5d133d452
Sturdyfool10 <etwright0658@pm.me>
2025-09-02 20:38:27 -0700

    fix main

--- crates/calendar_server/src/main.rs
+++ crates/calendar_server/src/main.rs
@@ -4,7 +4,8 @@ use global_constants::LOGS_PATH;
 use tracing::*;
 use webserver::start_web_server;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     logging::init_logging();
     info!("Initializing config...");
     let conf = ConfigMan::load_or_init_config("config.json");
